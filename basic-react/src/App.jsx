06b48901e372c89a1bd6a60d6c6800320341e614
Ankit Sinha <ankits.brainerhub@gmail.com>
2024-09-06 12:54:13 +0530

    Added the basic project

---
+++ basic-react/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>React Basic Tutorial</h1>
+        <h2>Welcome to basic react tutorial</h2>
+        <h2>Hello World</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
